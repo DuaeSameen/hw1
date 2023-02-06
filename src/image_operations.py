24340dde7b70bbaa8bb24b88af7e86c6c302034d
DuaeSameen <97940690+DuaeSameen@users.noreply.github.com>
2023-02-06 21:26:21 +0500

    Update image_operations.py

--- src/image_operations.py
+++ src/image_operations.py
@@ -62,4 +62,78 @@ def resize(src: MyImage) -> MyImage:
     Returns:
     an image twice the size of src.
     """
+    
+    
+    
+    big_image = MyImage((src.size[0]*2, src.size[1]*2))
+
+    for i in range(src.size[0]):
+        for x in range(src.size[1]):
+            big_image.set(i*2, x*2,src.get(i,x))
+
+    for i in range(src.size[0]*2):
+        for x in range(src.size[1]*2):
+            prev_col = src.size[1]*2-1
+            prev_row = src.size[0]*2-1
+
+            if (i%2 == 0 and x%2 !=0 and x!= prev_col):
+                pixel1 = big_image.get(i,(x-1))
+                pixel2 = big_image.get(i,(x+1))
+                redavg = (pixel1[0]+pixel2[0])//2
+                greenavg = (pixel1[1]+pixel2[1])//2
+                blueavg = (pixel1[2]+pixel2[2])//2
+                rgbvalue = (redavg,greenavg,blueavg)
+                big_image.set(i,x,rgbvalue)
+            elif (x==prev_col):
+                big_image.set(i,x,big_image.get(i,x-1))
+
+            if(i%2!= 0 and x%2==0 and i != prev_row):
+                pixel1 = big_image.get((i-1), x)
+                pixel2 = big_image.get((i+1), x)
+                redavg = (pixel1[0]+pixel2[0])//2
+                greenavg = (pixel1[1]+pixel2[1])//2
+                blueavg = (pixel1[2]+pixel2[2])//2
+                rgbvalue = (redavg,greenavg,blueavg)
+                big_image.set(i,x,rgbvalue)
+
+            elif (x == prev_col):
+                big_image.set(i,x,big_image.get(i,x-1))
+
+            if (i%2!= 0 and x%2==0 and i != prev_row and x!= prev_row):
+                pixel1=big_image.get((i-1),x)
+                pixel2=big_image.get((i+1),x)
+                redavg = (pixel1[0] +pixel2[0])//2
+                greenavg = (pixel1[1] +pixel2[1])//2
+                blueavg = (pixel1[2] +pixel2[2])//2
+                
+                rgbvalue = (redavg,greenavg,blueavg)
+                big_image.set(i,x,rgbvalue)
+
+
+            elif (i == prev_col):
+                big_image.set(i,x,big_image.get(i-1,x))
+
+            if (i%2!=0 and x%2!=0 and i!=prev_row and x!=prev_col):
+
+                
+                
+                pixel1 = big_image.get((i-1), (x-1))
+                pixel2 = big_image.get((i+1), (x+1))
+                pixel3 = big_image.get((i+1),(x-1))
+                pixel4 = big_image.get((i-1),(x+1))
+
+                redavg = (pixel1[0]+pixel2[0]+pixel3[0]+pixel4[0])//4
+                greenavg = (pixel1[1]+pixel2[1]+pixel3[1]+pixel4[1])//4
+                blueavg = (pixel1[2]+pixel2[2]+pixel3[2]+pixel4[2])//4
+                rgbvalue = (redavg,greenavg,blueavg)
+
+                big_image.set(i,x,rgbvalue)
+
+            elif(i==prev_row and x%2!=0 and x!= prev_col):
+                big_image.set(i,x,big_image.get(i-1,x))
+            elif(i!=prev_row and i%2!=0 and x== prev_col):
+                big_image.set(i,x,big_image.get(i,x-1))
+            elif(i==prev_row and x==prev_col):
+                big_image.set(i,x,big_image.get(i-1,x-1))
+    return big_image
     pass
